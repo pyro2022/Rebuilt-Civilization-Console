95000b663697d205ba97208218658013d13db231
pyro2022 <96921331+pyro2022@users.noreply.github.com>
2023-05-06 20:16:47 +0800

    Update Playerdata.java

--- src/main/java/com/prominent/crc/procdata/Playerdata.java
+++ src/main/java/com/prominent/crc/procdata/Playerdata.java
@@ -1,7 +1,7 @@
 package com.prominent.crc.procdata;
 
 public class Playerdata {
-    public Playerdata(int coin,String statement){//The 'String statement' is need to separate..
+    public Playerdata(int coin,String statement){//The 'String statement' is need to separate.
 
     }
 }
